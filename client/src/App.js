f2ffefa9dd1abd0fd6d15284180652d701afd537
deming16 <neverggyay@gmail.com>
2018-12-27 16:14:47 +0800

    logout user when token expires

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import './App.css';
 
 // Check for token
@@ -20,6 +20,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 

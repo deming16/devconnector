703f850c9a26d5e528b40e6c7f43872263a97baf
deming16 <neverggyay@gmail.com>
2018-12-27 15:20:24 +0800

    Integrated register from front to backend

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,33 @@
+import { GET_ERRORS } from '../actions/types';
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+  axios.post('/api/users/register/', userData)
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Login - Get User Token
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      // Save to local Storage
+      const { token } = res.data;
+      // Set token to ls
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }));
+};
